6a90286117cfc0d48d44b2c8db5f4ddfa520fee1
Kamil Gałdzicki <kamil.galdzicki@mebway.pl>
2022-01-25 00:17:03 +0100

    2_6

--- start/lecture2_6/app.js
+++ start/lecture2_6/app.js
@@ -33,7 +33,9 @@ class App{
 		container.appendChild( this.renderer.domElement );
 		
         //Add code here
-        
+        this.LoadingBar = new LoadingBar()
+        // this.loadGLTF();
+        this.loadFBX();
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.target.set(0, 3.5, 0);
@@ -62,9 +64,48 @@ class App{
     
     loadGLTF(){
         const self = this;
+        const loader = new GLTFLoader().setPath('../../assets/');
+
+        loader.load(
+            'office-chair.glb',
+            function(gltf){
+                self.chair = gltf.scene;
+                const bbox = new THREE.Box3().setFromObject( gltf.scene );
+                console.log(`min:${vector3ToString(bbox.min, 2)} - max:${vector3ToString(bbox.max, 2)}`);
+                self.scene.add( gltf.scene );
+                self.LoadingBar.visible = false;
+                self.renderer.setAnimationLoop( self.render.bind(self) );
+            },
+            function (xhr) {
+                self.LoadingBar.progress = xhr.loaded / xhr.total;
+            },
+            function (err) {
+                console.log( 'Error' );
+            }
+        )
     }
     
     loadFBX(){
+        const self = this;
+        const loader = new FBXLoader().setPath('../../assets/');
+
+        loader.load(
+            'office-chair.fbx',
+            function(object){
+                self.chair = object;
+                const bbox = new THREE.Box3().setFromObject( object );
+                console.log(`min:${vector3ToString(bbox.min, 2)} - max:${vector3ToString(bbox.max, 2)}`);
+                self.scene.add( object );
+                self.LoadingBar.visible = false;
+                self.renderer.setAnimationLoop( self.render.bind(self) );
+            },
+            function (xhr) {
+                self.LoadingBar.progress = xhr.loaded / xhr.total;
+            },
+            function (err) {
+                console.log( 'Error' );
+            }
+        )
     }
     
     resize(){
