c81b66533cc21d67d9881e78498dcdf69c0e9dcc
Kamil Gałdzicki <kamil.galdzicki@mebway.pl>
2022-01-27 22:31:26 +0100

    furniture module test

--- start/lecture2_5/app.js
+++ start/lecture2_5/app.js
@@ -5,13 +5,19 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
+
+        // LOADER
+        this.loader = new THREE.ObjectLoader();
         
+        // CAMERA
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
 		this.camera.position.set( 0, 0, 4 );
         
+        // SCENE
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xaaaaaa );
 
+        // LIGHTS
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
 		this.scene.add(ambient);
         
@@ -19,19 +25,24 @@ class App{
         light.position.set( 0.2, 1, 1);
         this.scene.add(light);
 			
+        // RENDERER
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( this.renderer.domElement );
 		
+        // MODEL
         const geometry = new THREE.TorusKnotBufferGeometry( 0.8, 0.3, 120, 16 ); 
         
         const material = new THREE.MeshPhongMaterial( { color: 0xff00ff, specular: 0x444444, shininess: 60 } );
 
         this.mesh = new THREE.Mesh( geometry, material );
         
+        // ADD MODEL
         this.scene.add(this.mesh);
         
+
+        // OTHER
         const controls = new OrbitControls( this.camera, this.renderer.domElement );
         
         this.renderer.setAnimationLoop(this.render.bind(this));
@@ -46,7 +57,7 @@ class App{
     }
     
 	render( ) {   
-        this.mesh.rotateY( 0.01 );
+        // this.mesh.rotateY( 0.01 );
         this.renderer.render( this.scene, this.camera );
     }
 }
